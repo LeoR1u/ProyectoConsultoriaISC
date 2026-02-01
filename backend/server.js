cc38c263da3b04bc2d534f87d8db3fbd114e5da9
Fernando <Lerf030509@outlook.com>
2026-02-01 12:26:34 -0600

    Modificacion para premisos de CORS.

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,16 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://consultoriatecs1102.netlify.app/'
+    'https://consultoriatecs1102.netlify.app'
   ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 
-app.use(cors(corsOptions));app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
